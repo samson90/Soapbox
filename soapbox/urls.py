f6d066be90b9a616d553187709ca54298f2a51c7
samson90 <cole.zuber@tufts.edu>
2014-01-20 22:11:06 -0500

    added urls

--- soapbox/urls.py
+++ soapbox/urls.py
@@ -4,9 +4,22 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'soapbox.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # Example:
+    # (r'^debatewebsite/', include('debatewebsite.foo.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+    url(r'^home/$', home),
+		url(r'^video-page/([a-zA-Z0-9_]*)$', video_page),
+		url(r'^profile/([a-zA-Z0-9_]*)$', profile),
+		url(r'^submit_video/$', submit_video),
+		
+		# Video debate urls
+		url(r'^email/$', email),
+    url(r'^invite/(\d+)$', invite),
+    url(r'^accept/(\d+)$', accept),
+    url(r'^debate/([a-zA-Z0-9_]*)$', debate),
 )
