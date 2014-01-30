cf066c03ad225308b94a93cab090031c86b2321c
samson90 <cole.zuber@tufts.edu>
2014-01-29 22:34:42 -0500

    uploading should be function

--- soapbox/settings.py
+++ soapbox/settings.py
@@ -89,12 +89,12 @@ STATIC_URL = '/static/'
 # Parse database configuration from $DATABASE_URL
 #COMMENT OUT LINES BELOW IF RUNNING LOCALLY
 ###############################
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
+#############################
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-#############################
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
